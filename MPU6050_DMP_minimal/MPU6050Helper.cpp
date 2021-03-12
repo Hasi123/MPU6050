73b0ec8eaea75124aef6f313565f28b40bb9e733
Hasi123 <Hasi123@users.noreply.github.com>
2021-03-12 20:27:36 +0100

    Initialize the sensor and load the compressed dmp
    Still need to implement FIFO reading

---
+++ MPU6050_DMP_minimal/MPU6050Helper.cpp
@@ -0,0 +1,156 @@
+#include <Arduino.h>
+#include "MPU6050Helper.h"
+#include "I2Cdev.h"
+//Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+#include "inv_dmp_uncompress.h"
+
+
+uint8_t readByte(uint8_t devAddr, uint8_t regAddr) {
+  uint8_t buff;
+  I2Cdev::readByte(devAddr, regAddr, &buff);
+  return buff;
+}
+
+bool writeByte(uint8_t devAddr, uint8_t regAddr, uint8_t buff) {
+  return I2Cdev::writeByte(devAddr, regAddr, buff);
+}
+
+int16_t readWord(uint8_t devAddr, uint8_t regAddr) {
+  uint16_t buff;
+  I2Cdev::readWord(devAddr, regAddr, &buff);
+  return buff;
+}
+
+bool writeWord(uint8_t devAddr, uint8_t regAddr, int16_t buff) {
+  return I2Cdev::writeWord(devAddr, regAddr, buff);
+}
+
+int8_t readBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint8_t *data) {
+  return I2Cdev::readBytes(devAddr, regAddr, length, data);
+}
+
+bool writeBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint8_t *data) {
+  return I2Cdev::writeBytes(devAddr, regAddr, length, data);
+}
+
+int16_t readWordAveraged(uint8_t devAddr, uint8_t regAddr, uint8_t loops) {
+  int32_t sum = 0;
+  for (uint8_t i = 0; i < loops; i++) {
+    sum += readWord(devAddr, regAddr);
+  }
+  return (int16_t)(sum / loops);
+}
+
+//Print hex number
+void printHex(uint8_t hexVal) {
+  Serial.print("0x");
+  if (hexVal < 0x10)
+    Serial.print(0);
+  Serial.print(hexVal, HEX);
+}
+
+//Dump all the MPUs regs
+void mpu_dump_regs() {
+  for (uint8_t i = 0; i < 255; i++) {
+    printHex(i);
+    Serial.print(", ");
+    printHex(readByte(mpuAddr, i));
+    Serial.println();
+  }
+}
+
+//Write to the DMP memory
+int8_t mpu_write_mem(uint16_t mem_addr, uint16_t length, uint8_t *data) {
+
+  if (!writeWord(mpuAddr, MPU6050_RA_BANK_SEL, mem_addr))
+    return -1;
+  if (!writeBytes(mpuAddr, MPU6050_RA_MEM_R_W, length, data))
+    return -1;
+  return 0;
+}
+
+//Read from the DMP memory
+int8_t mpu_read_mem(uint16_t mem_addr, uint16_t length, uint8_t *data) {
+
+  if (!writeWord(mpuAddr, MPU6050_RA_BANK_SEL, mem_addr))
+    return -1;
+  if (!readBytes(mpuAddr, MPU6050_RA_MEM_R_W, length, data))
+    return -1;
+  return 0;
+}
+
+//Load and verify DMP image
+int8_t load_dmp() {  //using compressed DMP firmware
+  //#define VERIFY_DMP //should we verify loading the DMP?
+
+  uint16_t ii, this_write;
+  uint8_t progBuffer[MPU6050_DMP_MEMORY_CHUNK_SIZE];
+#ifdef VERIFY_DMP
+  uint8_t cur[MPU6050_DMP_MEMORY_CHUNK_SIZE];
+#endif
+
+  /* start loading */
+  inv_dmp_uncompress_reset();
+
+  for (ii = 0; ii < UNCOMPRESSED_DMP_CODE_SIZE; ii += this_write) {
+    this_write = min(MPU6050_DMP_MEMORY_CHUNK_SIZE, UNCOMPRESSED_DMP_CODE_SIZE - ii);
+
+    /* decompress chunk */
+    for (uint16_t progIndex = 0; progIndex < this_write; progIndex++)
+      progBuffer[progIndex] = inv_dmp_uncompress();
+
+    //write
+    if (mpu_write_mem(ii, this_write, progBuffer))
+      return -1;
+#ifdef VERIFY_DMP //check
+    if (mpu_read_mem(ii, this_write, cur))
+      return -1;
+    if (memcmp(progBuffer, cur, this_write))
+      return -2;
+#endif
+  }
+
+  /* Set program start address. */
+  if (!writeWord(mpuAddr, MPU6050_RA_DMP_CFG_1, MPU6050_DMP_START_ADDRESS))
+    return -1;
+
+  return 0;
+}
+
+//Init the sensor and load dmp
+void mpuInit() {
+  //join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+  //following order is done in InvenSense example code
+  //redundant writes are deleted
+#define RESET_MPU //regs get reset on power cycle, so not really needed
+#ifdef RESET_MPU
+  writeByte(mpuAddr, MPU6050_RA_PWR_MGMT_1, 0b10000000); //reset
+  delay(100);
+#endif
+  writeByte(mpuAddr, MPU6050_RA_PWR_MGMT_1, MPU6050_CLOCK_PLL_XGYRO); //wake up and set clock to gyro X (recomended by datasheet)
+  writeByte(mpuAddr, MPU6050_RA_GYRO_CONFIG, MPU6050_GYRO_FS_2000 << 3); //Gyro range
+  writeByte(mpuAddr, MPU6050_RA_ACCEL_CONFIG, MPU6050_ACCEL_FS_2 << 3); //Accel range
+  writeByte(mpuAddr, MPU6050_RA_CONFIG, MPU6050_DLPF_BW_42); //DLPF
+#define SAMPLE_RATE 200 //has to be double as DMP rate
+  writeByte(mpuAddr, MPU6050_RA_SMPLRT_DIV, 1000 / SAMPLE_RATE - 1);  //sample rate divider
+  //writeByte(mpuAddr, MPU6050_RA_INT_ENABLE, 0); //disable interrupts, already 0 by default
+  writeByte(mpuAddr, MPU6050_RA_INT_PIN_CFG, 0x80); //setup interrupt pin
+  delay(50);
+  load_dmp();
+  //writeByte(mpuAddr, MPU6050_RA_FIFO_EN, 0); //disable FIFO, already 0 by default
+  writeByte(mpuAddr, MPU6050_RA_USER_CTRL, 0b00001100); //reset FIFO and DMP
+  delay(50);
+  writeByte(mpuAddr, MPU6050_RA_USER_CTRL, 0b11000000); //enable FIFO and DMP
+  writeByte(mpuAddr, MPU6050_RA_INT_ENABLE, 0x02); //hardware DMP interrupt needed? yes in this case else set to 0
+  //writeByte(mpuAddr, MPU6050_RA_FIFO_EN, 0); //already 0
+
+}
--- MPU6050_DMP_minimal/MPU6050_DMP_minimal.ino
+++ MPU6050_DMP_minimal/MPU6050_DMP_minimal.ino
@@ -1,11 +1,42 @@
 //minimal implementation with DMP
+#include "MPU6050Helper.h"
 
-#include "inv_dmp_uncompress.h"
+//#define PRINT_GYRO
+//#define PRINT_ACCEL
+//#define PRINT_QUAT
 
 void setup() {
+  Serial.begin(57600);
+  while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
+  mpuInit();
 }
 
 void loop() {
+  short gyro[3], accel_short[3], sensors;
+  unsigned char more;
+  long quat[4];
+  unsigned long sensor_timestamp;
 
+
+  if (bitRead(readByte(mpuAddr, MPU6050_RA_INT_STATUS), 1)) { //new DMP packet
+    //dmp_read_fifo(gyro, accel_short, quat, &sensor_timestamp, &sensors, &more); //TODO
+
+#ifdef PRINT_GYRO
+    Serial.print(gyro[0]); Serial.print("\t");
+    Serial.print(gyro[1]); Serial.print("\t");
+    Serial.println(gyro[2]);
+#endif
+#ifdef PRINT_ACCEL
+    Serial.print(accel_short[0]); Serial.print("\t");
+    Serial.print(accel_short[1]); Serial.print("\t");
+    Serial.println(accel_short[2]);
+#endif
+#ifdef PRINT_QUAT
+    Serial.print(quat[0]); Serial.print("\t");
+    Serial.print(quat[1]); Serial.print("\t");
+    Serial.print(quat[2]); Serial.print("\t");
+    Serial.println(quat[3]);
+#endif
+  }
 }
